a24a5b77abeb244c7d419a1cccd05e267f48a9fb
Rob Arnold <robarnold@cs.cmu.edu>
2011-05-29 02:34:53 +0000

    Imbue the randompicker module with some proper randomness.

--- randompicker.js
+++ randompicker.js
@@ -1,7 +1,7 @@
 module.exports = function randompicker(objects, propname) {
   var chance = Math.random();
   for (var i = 0; i < objects.length; i++) {
-    if (objects[i].chance < chance) {
+    if (chance < objects[i].chance) {
       return objects[i][propname];
     }
     chance -= objects[i].chance;

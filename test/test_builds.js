420adc73855c20b216b9645a0b2c4c74bc300d0a
Shawn Wilsher <me@shawnwilsher.com>
2011-05-29 20:53:03 -0700

    Adds the framework needed for testing success/warning/error events. Adds a test for testing the success event has the properties we care about. Adds json files for data for the success event test.

--- test/test_builds.js
+++ test/test_builds.js
@@ -1,6 +1,10 @@
 var builds = require("../builds.js");
 var pulse = require("pulse");
 var testCase = require("nodeunit").testCase;
+var http = require("http");
+var assert = require("assert");
+var fs = require("fs");
+var events = require("events");
 
 // Before we do anything, we need to modify BuildConsumer so that it does not
 // actually try to connect to pulse.
@@ -32,3 +36,59 @@ exports.test_Watcher_prototype = function(test) {
   test.ok(w instanceof events.EventEmitter);
   test.done();
 };
+
+exports.events = testCase({
+  setUp: function(callback)
+  {
+    // These tests will end up calling http.get to get data from some other
+    // service.  This test is responsible for establishing what is supposed to
+    // be returned, but we'll mock out get for them so they don't have to.
+    this._originalGet = http.get;
+
+    this.hosts = {};
+    http.get = function(options, callback) {
+      assert.ok(options.host in this.hosts);
+      var paths = this.hosts[options.host];
+      assert.ok(options.path in paths);
+
+      // Create a mock response object...
+      var res = new events.EventEmitter();
+      res.setEncoding = function() { };
+      callback(res);
+
+      // ...and then respond with our predefined data.
+      res.emit("data", paths[options.path]);
+      res.emit("end");
+      return new events.EventEmitter();
+    }.bind(this);
+    callback();
+  },
+  tearDown: function(callback)
+  {
+    http.get = this.originalGet;
+    callback();
+  },
+
+  test_success_event: function(test) {
+    test.expect(8);
+    this.hosts["hg.mozilla.org"] = {
+      "/try/json-pushes?changeset=e8cdbbadcd0a":
+        fs.readFileSync("json-pushes.e8cdbbadcd0a.json", "utf8"),
+    };
+    var w = new builds.Watcher("try");
+    w.on("success", function(build) {
+      test.ok(build.key);
+      test.equal(build.type, "mochitest-plain-2");
+      test.equal(build.platform, "macosx64");
+      test.equal(build.slave, "talos-r3-snow-037");
+      test.equal(build.rev, "e8cdbbadcd0a");
+      test.equal(build.result, builds.Watcher.kBuildbotSuccess);
+      test.equal(build.tree, "try");
+      test.equal(build.pusher, "async.processingjs@yahoo.com");
+      test.done();
+    });
+    var response = JSON.parse(fs.readFileSync("mochitest-plain-2.success.json",
+                                              "utf8"));
+    gBuildConsumer.emit("message", response);
+  },
+});
